04bddab9547f7515119c391f32a6f8f39663d2d2
GamerXWarrior <40907640+GamerXWarrior@users.noreply.github.com>
2018-07-07 12:14:34 +0530

    added code

---
+++ intern house vr/Assets/controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+	private Vector3 spawnPoint;
+	private bool walking ;
+	// Use this for initialization
+	void Start () {
+		spawnPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (walking) {
+			transform.position = transform.position + Camera.main.transform.forward * .5f * Time.deltaTime;
+		}
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+		
+			if (hit.collider.name.Contains ("plane")) {
+				walking = false;
+			} else { walking = true;
+			}
+			if (transform.position.y < -10f) {
+				transform.position = spawnPoint;}
+		}
+	}
+}
